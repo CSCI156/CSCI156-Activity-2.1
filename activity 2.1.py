9765900cb86bbab195dc3399088f4a9a6203359d
theweirdowombat <kaitlynmariecookis@gmail.com>
2020-12-29 14:57:03 -0500

    Initial Commit

---
+++ activity 2.1.py
@@ -0,0 +1,30 @@
+width = 16
+height = 12.0
+delimiter = '.'
+
+# width/2 would be an integer
+# width/2.0 would be an integer
+# height/3 would be an integer
+# 1+2*5 would be an integer
+# delimiter*5 would be a string
+
+print(width/2)
+print(width/2.0)
+print(height/3)
+print(1+2*5)
+print(delimiter*5)
+
+print(8/2)
+print(int(8/2))
+print(int(9/2))
+print(int(9.2/2))
+
+variable = 'a'
+print(variable*25)
+
+my = "Kaitlyn"
+name = "My name is"
+print(name+my)
+
+print("Hello"
+      "World!")
